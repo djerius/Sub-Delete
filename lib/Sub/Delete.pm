6423f465e5af4484b455c380a09a2080b89b6ee5
Diab Jerius <djerius@cfa.harvard.edu>
2024-01-25 14:20:59 -0500

    replace apostrophe with double colon in package name, as using apostrophe as a package separator is deprecated as of Perl 5.38

--- lib/Sub/Delete.pm
+++ lib/Sub/Delete.pm
@@ -4,7 +4,7 @@ package Sub::Delete;
 
 $VERSION = '1.00002';
 @EXPORT = delete_sub;
-use Exporter 5.57 'import';
+use Exporter 5.57 'import';
 use constant point0 => 0+$] eq 5.01;
 
 # This sub must come before any lexical vars.
@@ -44,7 +44,7 @@ sub delete_sub {
 	}
         delete $stash->{$key};
 	keys %imported_slots == 1 and exists $imported_slots{SCALAR}
-	 and !$imported_slots{SCALAR} and Internals'SvREFCNT $$glob =>== 1
+	 and !$imported_slots{SCALAR} and Internals::SvREFCNT $$glob =>== 1
 	 and !defined *$glob{IO} and !defined *$glob{FORMAT}
 	 and return; # empty glob
 	my $newglob = \*$globname;
